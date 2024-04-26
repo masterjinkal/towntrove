b6c4a67ff5df1d3cc1f6e82a1ac47a2ad6a626e5
Jinkal Master <masterjinkal123@gmail.com>
2024-04-27 00:42:27 +0530

    changed homepage image

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,14 +9,14 @@ export const Hero = () => {
   return (
     <div className='hero'>
         <div className="hero-left">
-            <h2>NEW ARRIVALS ONLY</h2>
+            <h2>Discover Local Delights!</h2>
             <div>
                 <div className="hero-hand-icon">
-                    <p>new</p>
+                    <p>Supporting</p>
                     <img src = {hand_icon} alt = ""/>
                 </div>
-                <p>collections</p>
-                <p>for everyone</p>
+                <p>Local Businesses,</p>
+                <p>One Click at a Time</p>
             </div>
             <div className="hero-latest-btn">
                 <div>Latest Collection</div>
@@ -24,7 +24,7 @@ export const Hero = () => {
             </div>
         </div>
         <div className="hero-right">
-            <img src={hero_image} alt = ""/>
+            <img src = {hero_image} alt = ""/>
         </div>
     </div>
   )
